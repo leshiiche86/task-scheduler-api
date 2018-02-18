028347d054d425da52109bc6f821798c26b62354
 Алексей Черебаев <aleksejcerebaev@iMac-Aleksej.local>
2018-02-18 18:01:51 +0600

    Add auth with jwt token

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 import auth from './routes/user';
 
+dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost/taskscheduler');
+mongoose.connect(process.env.MONGODB_URL);
 
 app.use('/api/auth', auth);
 
